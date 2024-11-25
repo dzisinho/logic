bf73e30784f76223f69672209c20b468f4b00980
dzisinho <dzisbogdan26@gmail.com>
2024-11-25 21:49:44 +0200

    completed

--- logic.cpp
+++ logic.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cin >> num;
+
+    if (num < 100000 || num > 999999) {
+        cout << "Error: - a 6 number\n";
+    }
+    else {
+        int firstSum = (num / 100000) + (num / 10000 % 10) + (num / 1000 % 10);
+        int secondSum = (num / 100 % 10) + (num / 10 % 10) + (num % 10);
+
+        if (firstSum == secondSum) {
+            cout << "Lucky number\n";
+        }
+        else {
+            cout << "Not a lucky number\n";
+        }
+    }
+
+    return 0;
+}
