55bb9fe58ccc8bf1d4720217e94fa9fb3d4f0578
dzisinho <dzisbogdan26@gmail.com>
2024-11-25 22:08:47 +0200

    completed

--- logic.cpp
+++ logic.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int num, maxNum;
+    cin >> maxNum; 
+
+    for (int i = 1; i < 7; ++i) {
+        cin >> num;
+        if (num > maxNum) {
+            maxNum = num;
+        }
+    }
+
+    cout << "Maximum number: " << maxNum << "\n";
+
+    return 0;
+}
